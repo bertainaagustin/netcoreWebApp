39011e50479b41d0afbd30c061bd93c71a98ce08
bertainaagustin <flormmurano@gmail.com>
2019-07-18 21:50:34 -0300

    AddItems

--- AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Services/TodoItemService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreTodo.Data;
@@ -14,11 +15,25 @@ namespace AspNetCoreTodo.Services
         {
             _context = context;
         }
+
+    
+
         public async Task<TodoItem[]> GetIncompleteItemsAsync()
         {
-            return await _context.Items.ToArrayAsync();
-                //.Where(x => x.IsDone == false)
-                //.ToArrayAsync();
+            return await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+        }    
+        public async Task<bool> AddItemAsync(TodoItem newItem)
+        {
+            newItem.Id = Guid.NewGuid();
+            newItem.IsDone = false;
+            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+            _context.Items.Add(newItem);
+
+            var saveResult = await _context.SaveChangesAsync();
+            return saveResult == 1;
         }
     }
 }

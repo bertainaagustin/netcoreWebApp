032c92a866cbb18a26a9cfbb2f975c147c2578de
Agustin Bertaina <bertainaagustin@gmail.com>
2019-08-29 12:24:14 -0300

    Adding first integration Test and new unit tests

--- AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
+++ AspNetCoreTodo/AspNetCoreTodo.UnitTests/TodoItemServiceShould.cs
@@ -38,10 +38,126 @@ namespace AspNetCoreTodo.UnitTests
                 Assert.Equal(1, itemsInDatabase);
                 var item = await context.Items.FirstAsync();
                 Assert.Equal("Testing?", item.Title);
-                Assert.Equal(false, item.IsDone);
+                Assert.False(item.IsDone);
                 // Item should be due 3 days from now (give or take a second)
                 var difference = DateTimeOffset.Now.AddDays(3) - item.DueAt;
                 Assert.True(difference < TimeSpan.FromSeconds(1));
+            }
+        }
+
+        [Fact]
+        public async Task MarkDoneAsyncCorrect()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_MarkDone_Correct").Options;
+            var fakeUser = new ApplicationUser
+            {
+                Id = "fake-000",
+                UserName = "fake@example.com"
+            };
+            var fakeItem =  new TodoItem
+            {
+                Title = "Testing?",
+                DueAt = DateTimeOffset.Now.AddDays(3)
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                await service.AddItemAsync(fakeItem, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var item = await context.Items.FirstAsync();
+
+                var result = await service.MarkDoneAsync(item.Id, fakeUser);
+                Assert.True(result);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var item = await context.Items.FirstAsync();
+                Assert.True(item.IsDone);
+            }
+        }
+
+        [Fact]
+        public async Task MarkDoneAsyncIncorrectId()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_MarkDone_Error").Options;
+            var fakeUser = new ApplicationUser
+            {
+                Id = "fake-000",
+                UserName = "fake@example.com"
+            };
+            var fakeItem =  new TodoItem
+            {
+                Title = "Testing?",
+                DueAt = DateTimeOffset.Now.AddDays(3)
+            };
+            var fakeItemId = Guid.NewGuid();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                await service.AddItemAsync(fakeItem, fakeUser);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                var item = await context.Items.FirstAsync();
+
+                var result = await service.MarkDoneAsync(fakeItemId, fakeUser);
+                Assert.False(result);
+            }
+        }
+
+        [Fact]
+        public async Task GetIncompleteItemsAsyncUserCheck() 
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "Test_GetIncompleteItems").Options;
+            var fakeUser1 = new ApplicationUser
+            {
+                Id = "fake-000",
+                UserName = "fake@example.com"
+            };
+            
+            var fakeUser2 = new ApplicationUser 
+            {
+                Id = "fake-999",
+                UserName = "fake2@example.com"
+            };
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?",
+                    DueAt = DateTimeOffset.Now.AddDays(3)
+                }, fakeUser1);
+
+                await service.AddItemAsync(new TodoItem
+                {
+                    Title = "Testing?",
+                    DueAt = DateTimeOffset.Now.AddDays(3)
+                }, fakeUser2);
+            }
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var service = new TodoItemService(context);
+
+                var items = await service.GetIncompleteItemsAsync(fakeUser1);
+
+                foreach(var item in items)
+                {
+                    Assert.Equal(item.UserId, fakeUser1.Id);
+                }
             }
         }
     }
